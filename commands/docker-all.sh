dc0b330ba1e23f949bbddfbfe7e8f9b706d89437
romain jouin <romain.jouin@gmail.com>
2017-02-19 00:06:42 +0100

    adding some big data hdfs / flume tool configuration files

---
+++ commands/docker-all.sh
@@ -0,0 +1,1 @@
+docker run --net host --publish-all=true -it --rm  -v ~/Informatique/velib:/home/jovyan/work jupyter/all-spark-notebook  start-notebook.sh --NotebookApp.token='velib'
---
+++ commands/flume_cycliste_debug.sh
@@ -0,0 +1,1 @@
+./bin/flume-ng agent --conf ./conf/ -f ./conf/cycliste_debug.conf -Dflume.root=DEBUG,console -n agent1
